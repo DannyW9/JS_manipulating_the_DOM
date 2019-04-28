e630993e9077a8b98859d1b7a14bd3b423924caf
Danny Welsh <dannywelsh400@hotmail.com>
2019-04-28 20:36:32 +0100

    adds CSS and changes HTML to include radio buttons and a table

--- js/app.js
+++ js/app.js
@@ -3,6 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#new-animal');
   const speciesList = document.querySelector('#species-list');
   const deleteButton = document.querySelector('#delete');
+  const table = document.querySelector('#tbody');
+
+  // form.addEventListener('submit', (event) => {
+  //   event.preventDefault();
+  //
+  //   const speciesInput = event.target.species.value;
+  //   const continentInput = event.target.continent.value;
+  //   const statusInput = event.target.status.value;
+  //
+  //   const newEntry = document.createElement('p');
+  //   newEntry.textContent = `${speciesInput} -- ${continentInput} -- ${statusInput}`;
+  //   newEntry.classList.add('entry');
+  //
+  //   speciesList.appendChild(newEntry);
+  //   form.reset();
+  // })
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -11,18 +27,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const continentInput = event.target.continent.value;
     const statusInput = event.target.status.value;
 
-    const newEntry = document.createElement('p');
-    newEntry.textContent = `${speciesInput} -- ${continentInput} -- ${statusInput}`;
-    newEntry.classList.add('entry');
+    const row = document.createElement('tr');
+    const td1 = row.appendChild(document.createElement('td'));
+    const td2 = row.appendChild(document.createElement('td'));
+    const td3 = row.appendChild(document.createElement('td'));
 
-    speciesList.appendChild(newEntry);
+    td1.textContent = speciesInput;
+    td2.textContent = continentInput;
+    td3.textContent = statusInput;
+
+    table.appendChild(row);
     form.reset();
   })
 
+  // deleteButton.addEventListener('click', () => {
+  //   while (speciesList.firstChild) {
+  //     speciesList.removeChild(speciesList.firstChild);
+  //   }
+  // });
 
   deleteButton.addEventListener('click', () => {
-    while (speciesList.firstChild) {
-      speciesList.removeChild(speciesList.firstChild);
+    while (table.firstChild) {
+      table.removeChild(table.firstChild);
     }
   });
 
